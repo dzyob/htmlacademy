ee9c86b32f09865099032598ce863b313e4046bd
dzyob <dzyob01@gmail.com>
2021-08-31 16:15:55 +0300

    replace buttonTheme

--- htmlacademy/switchTheme.js
+++ htmlacademy/switchTheme.js
@@ -2,10 +2,10 @@ let buttonTheme = document.querySelector('.buttonTheme')
 buttonTheme.onclick = function () {
     if (!buttonTheme.classList.contains('nextTheme')) {
         document.getElementById("styleLink").href = "basic-dark-theme/style.css";
-        document.getElementById("buttonTheme1").style = "background-color: #ceeaff; color: #260c26; height: 20px; width: 20px;";
+        document.getElementById("buttonTheme1").style = "background-color: #ceeaff; color: #260c26; height: 20px; width: 20px; margin-left: 10px;";
     } else {
         document.getElementById("styleLink").href = "basic-light-theme/style.css";
-        document.getElementById("buttonTheme1").style = "background-color: #260c26; color: #ceeaff; height: 20px; width: 20px;";
+        document.getElementById("buttonTheme1").style = "background-color: #260c26; color: #ceeaff; height: 20px; width: 20px; margin-left: 10px;";
     }
     buttonTheme.classList.toggle('nextTheme');
     console.log(buttonTheme.classList.contains('nextTheme'));
