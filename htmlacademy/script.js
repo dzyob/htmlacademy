81c9d907cc79d2cc6e056712d507cecb0479b375
dzyob <dzyob01@gmail.com>
2021-08-31 14:00:19 +0300

    123

--- htmlacademy/script.js
+++ htmlacademy/script.js
@@ -1,5 +1,5 @@
 let themeButton = document.querySelector('.themeButton')
 themeButton.onClick = function () {
     var cssLink = $('link[href*="basic-light-theme/style.css"]');
-    cssLink.replaceWith('<link href="basic-dark-theme/style.css" type="text/css" rel="stylesheet">');
+    cssLink.replaceWith('<link href="basic-dark-theme/style.css">');
 }
