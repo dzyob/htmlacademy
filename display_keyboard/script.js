e56f98e492037835fe979ec376057dbb096002bf
dzyob <dzyob01@gmail.com>
2021-09-01 14:54:51 +0300

    add "enter" button

--- display_keyboard/script.js
+++ display_keyboard/script.js
@@ -1,7 +1,12 @@
 let keys = document.querySelectorAll('.key');
 let display = document.querySelector('.display');
 let clearButton = document.querySelector('.clear');
+let enter = document.querySelector('.enter');
 
+enter.onclick = function () {
+    let breaks = document.createElement('br');
+    display.append(breaks);
+}
 
 for (let key of keys) {
     key.onclick = function () {
